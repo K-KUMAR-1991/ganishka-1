aca731b3e93a3ad00c9c330a8c34965ed62e6ae0
K-KUMAR-1991 <142291435+K-KUMAR-1991@users.noreply.github.com>
2023-11-02 14:01:21 +0530

    Update App.java

--- aws-maven/src/main/java/in/K-KUMAR-1991/App.java
+++ aws-maven/src/main/java/in/K-KUMAR-1991/App.java
@@ -3,6 +3,6 @@ package in.K-KUMAR-1991;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!") ;
+        System.out.println("Hello World!");
     }
 }
